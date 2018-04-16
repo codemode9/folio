f05ef7ecb918e4546d859b9703880be59a7d411b
codemode9 <timourbasegmez@hotmail.com>
2018-04-16 16:26:22 -0400

    more

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -7,20 +7,20 @@ import "./Nav.css";
 const Nav = props => (
 <div className="container">
 <nav className="navbar navbar-expand-lg navbar-light">
-  <a className="navbar-brand nameOfMe" style={{color: 'white'}} href="#">Timour</a>
+  <a className="navbar-brand" style={{color: 'white'}} href="#">Timour</a>
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
   </button>
   <div className="collapse navbar-collapse" id="navbarText">
     
-    <span className="navbar-text">
-      <a className="nav-link" href="#">About</a>
+    <span className="navbar-text ml-auto">
+      <a className="nav-link" style={{color: 'white'}} href="#">About</a>
     </span>
     <span className="navbar-text">
-      <a className="nav-link" href="#">Portfolio</a>
+      <a className="nav-link" style={{color: 'white'}} href="#">Portfolio</a>
     </span>
     <span className="navbar-text">
-      <a className="nav-link" href="#">Contact</a>
+      <a className="nav-link" style={{color: 'white'}} href="#">Contact</a>
     </span>
   </div>
 </nav>
