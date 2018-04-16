57a27fdeac750f954a53a26bfb5afcc437c3bf6a
codemode9 <timourbasegmez@hotmail.com>
2018-04-16 18:15:11 -0400

    component work

---
+++ src/components/About/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+import {Link} from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.css';
+import "./Nav.css";
+
+const About = props => (
+<div className="container">
+
+</div>
+);
+
+export default About;
