27e9bf9d29dd3d02ff064ad07ec17e16a866f197
codemode9 <timourbasegmez@hotmail.com>
2018-04-16 20:05:23 -0400

    working on new component + wallpaper tests

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -5,8 +5,8 @@ import "./About.css";
 
 const About = props => (
 <div className="container">
-    <div className="card" style={{width: '18rem'}}>
-        <img className="card-img-top" src=".../100px180/" alt="Card image cap" />
+    <div className="card" style={{width: '34rem'}}>
+        <img className="card-img-top" src={require("../../pics/profilepic.png")} alt="Card image cap" style={{width: '18rem'}} />
         <div className="card-body">
           <h5 className="card-title">About</h5>
           <p className="card-text">Bacon ipsum dolor amet cow turducken ground round sausage boudin meatball salami pork loin frankfurter tri-tip ball tip. Beef ribs prosciutto buffalo, sirloin cupim ribeye ground round burgdoggen. Corned beef short loin strip steak tri-tip, tail rump ham hock cupim kielbasa sausage tongue fatback capicola. Bacon pancetta alcatra, flank andouille filet mignon turkey salami jerky kevin capicola cow rump leberkas. Shankle ball tip hamburger spare ribs burgdoggen jowl rump ribeye frankfurter t-bone prosciutto meatloaf. Leberkas burgdoggen t-bone strip steak tri-tip meatloaf. Rump ribeye filet mignon meatball.
